2fa02fa14c86ee2c16ea57987ef0a5b4ec7a5007
wanghaiyang <wanghaiyang@btte.net>
2019-11-30 12:37:54 +0800

    add reformer for Demo

--- app/src/main/java/com/haiyangwang/summer/MainActivity.java
+++ app/src/main/java/com/haiyangwang/summer/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 
+import com.haiyangwang.summer.HomePage.FilmDataKey;
+import com.haiyangwang.summer.HomePage.FilmDataReformer;
 import com.haiyangwang.summer.HomePage.HomePageApiManager;
 import com.haiyangwang.summer.NetWork.InterfaceDefines.ApiManagerResultCallBackDelegate;
 import com.haiyangwang.summer.NetWork.VVBaseApiManager;
+import com.haiyangwang.summer.NetWork.VVURLResponse;
 import com.haiyangwang.summer.SearchBar.SearchBar;
 import com.haiyangwang.summer.SearchBar.SearchView;
 import com.haiyangwang.summer.ShufflingView.ShufflingItem;
@@ -16,12 +19,11 @@ import com.haiyangwang.summer.ShufflingView.ShufflingView;
 
 
 import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements
-        ApiManagerResultCallBackDelegate,
-        ShufflingView.ShufflingViewDataSource,
-        ShufflingView.ShufflingViewDelegate,
-        SearchBar.SearchLogic {
+        ApiManagerResultCallBackDelegate{
 
     private static final String TAG = "MainActivity";
     private HomePageApiManager homePageApiManager;
@@ -35,73 +37,45 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // loadData
         getHomePageApiManager().loadData();
 
-        mShufflingView = findViewById(R.id.shuffling);
-        mSearchBar = findViewById(R.id.search_bar);
-
-        mSearchView = new SearchView(this);
-        mSearchBar.setSearchUI(new WeakReference<>(mSearchView));
-        mSearchBar.setSearchLogic(new WeakReference<>(this));
-
-        mShufflingView.setDataSource(new WeakReference<>(this));
-        mShufflingView.loadData();
-        mShufflingView.setDelegate(new WeakReference<>(this));
-
-
     }
 
-    @Override
-    public int getItemCount() {
-        return 9;
-    }
-
-    @Override
-    public ShufflingItem getItemView(int itemIndex, @Nullable ShufflingItem item) {
-
-        ShufflingItem shufflingItem = null;
-        if (item != null) {
-            shufflingItem = item;
-        }else {
-            shufflingItem = new ShufflingItem(this);
-        }
-        shufflingItem.setIdentifier("我的数据是"+itemIndex);
-        return shufflingItem;
-    }
-
-    @Override
-    public void itemBeClickedAtIndex(int index) {
-        Log.d(TAG,"item 选中了"+index);
-    }
-
-    @Override
-    public void doSearchWithText(String text) {
-        Log.d(TAG,"搜搜"+text);
-    }
 
     @Override
     public void managerCallApiDidSuccess(VVBaseApiManager manager) {
-        boolean flag = manager.equals(homePageApiManager);
         if (manager == homePageApiManager) {
-            Object data = manager.fetchResponseDataWithReformer(null);
-            Log.d(TAG, "managerCallApiDidSuccess: "+data);
+            Map<String, Object> dataMap = (Map<String, Object>) manager.fetchResponseDataWithReformer(new FilmDataReformer());
 
-        }
-        
-        if (manager.getClass() == HomePageApiManager.class) {
-            Log.d(TAG, "managerCallApiDidSuccess: this is a Home class");
+            List filmList = (List) dataMap.get(FilmDataKey.FilmInfo);
+            // test
+            Map<String, String> filmInfo = (Map<String, String>) filmList.get(0);
+            String filmName = filmInfo.get(FilmDataKey.FilmName);
+            String filmId = filmInfo.get(FilmDataKey.FilmID);
+            String filmImageAdr = filmInfo.get(FilmDataKey.FilmImageAddress);
+
+
+            Log.d(TAG, "managerCallApiDidSuccess: 影片信息："+"\n"+"name:"+filmName+"\n"
+                    +"id:"+filmId+"\n"
+                    +"imageAddress:"+filmImageAdr+"\n");
+
+
         }
     }
 
     @Override
     public void managerCallApiManagerFaild(VVBaseApiManager manager) {
-        Log.d(TAG, "managerCallApiManagerFaild: ");
         if (manager == homePageApiManager) {
-            String faildReason = manager.fetchFailureReason();
+            String faildReason = manager.fetchLog();
+            String faildType = manager.fetchFailureType();
             Log.d(TAG, "managerCallApiManagerFaild: "+faildReason);
         }
     }
 
+
+    // Home Api Manager
     public HomePageApiManager getHomePageApiManager() {
         if (homePageApiManager == null) {
             homePageApiManager = new HomePageApiManager();
@@ -110,3 +84,14 @@ public class MainActivity extends AppCompatActivity implements
         return  homePageApiManager;
     }
 }
+
+/*   mShufflingView = findViewById(R.id.shuffling);
+        mSearchBar = findViewById(R.id.search_bar);
+
+        mSearchView = new SearchView(this);
+        mSearchBar.setSearchUI(new WeakReference<>(mSearchView));
+        mSearchBar.setSearchLogic(new WeakReference<>(this));
+
+        mShufflingView.setDataSource(new WeakReference<>(this));
+        mShufflingView.loadData();
+        mShufflingView.setDelegate(new WeakReference<>(this));*/
